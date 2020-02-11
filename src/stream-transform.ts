622d7ac03a31b4f747df1b0118a33e7eb0479e7b
einsenhorn <einsenhorn@gmail.com>
2020-02-12 00:15:31 +0100

    Add StreamTransform
    Forgot to add it in the previous commit..

---
+++ src/stream-transform.ts
@@ -0,0 +1,26 @@
+import { Transform } from 'stream'
+
+export default {
+    /**
+     * @param current - current transform
+     * @param count - number of transform created
+     */
+    createTransform: (current: number, count: number): Transform => {
+        return new Transform({
+            transform (chunk: Buffer, _, callback) {
+                if (!Buffer.isBuffer(chunk)) {
+                    return callback(new Error('Chunk is not a buffer.'))
+                }
+
+                const length: number = chunk.length > count ? Math.round((chunk.length / count) + (chunk.length % count === current + 1 ? 1 : 0)) : 1
+                const buffer: Buffer = Buffer.alloc(length, 0)
+
+                for (let i = 0; i < length; i++) {
+                    buffer[i] = chunk[i * count + current]
+                }
+
+                callback(null, buffer)
+            }
+        })
+    }
+}
